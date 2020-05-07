f3b51190911ae479a9d078b5a0eb8139dda22f0b
Sahej Singh <sahejdeep@hotmail.com>
2020-05-07 22:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ function happyHolidays() {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 
 function happyCustomHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
+
+function holidayCountdown(holiday, days) {return `It's ${days} until ${holiday}`}
